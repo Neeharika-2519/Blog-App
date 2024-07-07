3b110347b9803e235c58578b63ceb6688b4f1f9a
Neeharika <neeharika.vijaya@gmail.com>
2024-07-07 15:00:01 -0400

    second commit

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import commentRoutes from './routes/comment.route.js';
 import postRoutes from './routes/post.route.js';
@@ -18,6 +19,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/blog',{
     console.log('MongoDb is Connected...')
 })
 
+const __dirname=path.resolve()
+
 const app=express();
 app.use(express.json());
 app.use(cookieParser())
@@ -26,6 +29,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/user',userRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.listen(3000,()=>{
     console.log('Server is running on 3000...')
